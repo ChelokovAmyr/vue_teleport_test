73202e2f407b6237e287ffbf605fc73b1c949eee
Amyr <44586350+ChelokovAmyr@users.noreply.github.com>
2022-05-09 08:36:03 +0300

    commit
    smain commit

---
+++ modals-n-portals/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page p-8">
+    <button
+        type="button"
+        @click="showModal = !showModal"
+        class="mx-auto w-full flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+    >
+      Open Modal
+    </button>
+    <ModalDialog :show="showModal" @close="showModal = false">
+      <p class="mb-4">Gokuu is...</p>
+      <img src="https://i.gifer.com/QjMQ.gif" />
+    </ModalDialog>
+  </div>
+</template>
+
+<script>
+import ModalDialog from './components/ModalDialog.vue';
+import { ref } from 'vue';
+const components = {
+  ModalDialog,
+};
+export default {
+  name: 'App',
+  components,
+  setup() {
+    const showModal = ref(false);
+    return {
+      showModal,
+    };
+  },
+};
+</script>
+
+<style></style>
